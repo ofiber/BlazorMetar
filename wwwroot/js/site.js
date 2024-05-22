60b2ab79471670e71b5bee97642d97339614e3e1
ofiber <harleyo@me.com>
2024-05-22 15:24:02 -0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿
+var textarea = document.getElementById("metarTb");
+
+
+
+function autoResize() {
+
+    textarea.style.minWidth = "100px";
+
+    textarea.style.width = "auto";
+    textarea.style.width = textarea.value.length * 15.5 + "px";
+}
+
+function showResults() {
+    var mainView = document.getElementById("mainView");
+
+    var resultsView = document.getElementById("resultsView");
+
+    mainView.style.visibility = "hidden";
+    resultsView.style.visibility = "visible";
+}
+
+function logMetar(metar) {
+    console.log(metar);
+}
